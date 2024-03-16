70d0c2ab67516cab06b57dd5902ff5e63c86358f
m4gen <alessrodriguez98@gmail.com>
2024-03-16 17:54:01 -0400

    v1

--- app/src/main/java/com/arr/bancamovil/utils/backups/BackupFile.java
+++ app/src/main/java/com/arr/bancamovil/utils/backups/BackupFile.java
@@ -27,6 +27,7 @@ import javax.crypto.spec.SecretKeySpec;
 public class BackupFile {
 
     private final Context mContext;
+    private Activity a;
 
     public BackupFile(Context context) {
         this.mContext = context;
